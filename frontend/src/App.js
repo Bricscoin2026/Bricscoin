10d5c60a1e74d0af7d89995abec8cadae50340ae
Bricscoin2026 <bricscoin2026@users.noreply.github.com>
2026-01-25 02:46:12 +0000

    auto-commit for 779b6c82-7183-481b-952c-a5514a37b0ca

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
-import { LanguageProvider } from "./context/LanguageContext";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import Explorer from "./pages/Explorer";
@@ -15,26 +14,24 @@ import "./App.css";
 
 function App() {
   return (
-    <LanguageProvider>
-      <div className="App noise-overlay min-h-screen bg-background">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Dashboard />} />
-              <Route path="explorer" element={<Explorer />} />
-              <Route path="block/:index" element={<BlockDetail />} />
-              <Route path="tx/:txId" element={<TransactionDetail />} />
-              <Route path="wallet" element={<Wallet />} />
-              <Route path="mining" element={<Mining />} />
-              <Route path="network" element={<Network />} />
-              <Route path="downloads" element={<Downloads />} />
-              <Route path="node" element={<RunNode />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-        <Toaster position="bottom-right" richColors />
-      </div>
-    </LanguageProvider>
+    <div className="App noise-overlay min-h-screen bg-background">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="explorer" element={<Explorer />} />
+            <Route path="block/:index" element={<BlockDetail />} />
+            <Route path="tx/:txId" element={<TransactionDetail />} />
+            <Route path="wallet" element={<Wallet />} />
+            <Route path="mining" element={<Mining />} />
+            <Route path="network" element={<Network />} />
+            <Route path="downloads" element={<Downloads />} />
+            <Route path="node" element={<RunNode />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Toaster position="bottom-right" richColors />
+    </div>
   );
 }
 
