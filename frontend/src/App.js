576fe41daf26886c5975a7978c1dadf42079181d
Bricscoin2026 <bricscoin2026@users.noreply.github.com>
2026-02-22 21:08:41 +0000

    auto-commit for c20e27d4-6c62-4978-82be-dd87df643611

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
-import Explorer from "./pages/Explorer";
+import Blockchain from "./pages/Blockchain";
 import BlockDetail from "./pages/BlockDetail";
-import Wallet from "./pages/Wallet";
-import Mining from "./pages/Mining";
-import Network from "./pages/Network";
 import TransactionDetail from "./pages/TransactionDetail";
+import WalletHub from "./pages/WalletHub";
 import Downloads from "./pages/Downloads";
-import RunNode from "./pages/RunNode";
 import About from "./pages/About";
-// Pools page removed
-import RichList from "./pages/RichList";
-import PQCWallet from "./pages/PQCWallet";
-import WalletMigration from "./pages/WalletMigration";
 import BricsChat from "./pages/BricsChat";
 import TimeCapsule from "./pages/TimeCapsule";
 import AiOracle from "./pages/AiOracle";
@@ -27,21 +20,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Dashboard />} />
-            <Route path="explorer" element={<Explorer />} />
+            <Route path="blockchain" element={<Blockchain />} />
             <Route path="block/:index" element={<BlockDetail />} />
             <Route path="tx/:txId" element={<TransactionDetail />} />
-            <Route path="wallet" element={<Wallet />} />
-            <Route path="pqc-wallet" element={<PQCWallet />} />
-            <Route path="migrate" element={<WalletMigration />} />
-            <Route path="mining" element={<Mining />} />
-            <Route path="network" element={<Network />} />
-            <Route path="downloads" element={<Downloads />} />
-            <Route path="node" element={<RunNode />} />
-            <Route path="richlist" element={<RichList />} />
-            <Route path="about" element={<About />} />
+            <Route path="wallet" element={<WalletHub />} />
             <Route path="chat" element={<BricsChat />} />
             <Route path="timecapsule" element={<TimeCapsule />} />
             <Route path="oracle" element={<AiOracle />} />
+            <Route path="downloads" element={<Downloads />} />
+            <Route path="about" element={<About />} />
           </Route>
         </Routes>
       </BrowserRouter>
